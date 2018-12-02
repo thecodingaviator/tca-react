8b0cb9a7dc531e4774c4a4465abf79abc871ffbd
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2018-12-02 20:51:45 +0530

    Analytics

--- public/index.js
+++ public/index.js
@@ -1,2 +1,6 @@
-/* eslint-disable */
+/* Analytics */
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
 
+gtag('config', 'UA-130237896-1');
