a33194b04fdb52c03d11f34accfcd4caff5677e9
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2018-12-02 14:57:19 +0530

    h1 fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <canvas></canvas>
         <div className="welcome">
           <h1>The Coding Aviator</h1>
           <h3>DEVELOPER | DESIGNER</h3>
