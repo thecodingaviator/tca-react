0bc6c32d2e147d204d93e65912be1ccf6204051e
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2018-12-16 17:16:16 +0530

    Update Welcome.js

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -11,6 +11,7 @@ export default class Welcome extends React.Component {
           <h1>THE CODING AVIATOR</h1>
           <div>
             <p>Tap/click and drag ;&#41;</p>
+            <br />
             <div className="down-arrow">
               <div className="chevron"></div>
               <div className="chevron"></div>
