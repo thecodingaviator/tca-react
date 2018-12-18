5d42d1bd013c67f32159c16bbdf2f955c7fcc757
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2018-12-18 21:21:37 +0530

    Update Contact.js

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -6,20 +6,10 @@ export default class Contact extends React.Component {
     return (
       <div className="contact">
         <h1>Reach Me</h1>
-        <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+        <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+          <input type="text" name="name" />
+          <input type="email" name="email" />
+          <textarea name="message"></textarea>
         </form>
       </div>
     )
