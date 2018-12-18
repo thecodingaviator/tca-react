f3eb74524438d45c4c83d46b6f351901bd911413
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2018-12-18 21:35:43 +0530

    Update Contact.js

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -6,7 +6,9 @@ export default class Contact extends React.Component {
     return (
       <div className="contact">
         <h1>Reach Me</h1>
-        <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
+        <section>
+                <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
+
                     <input type="hidden" name="bot-field" />
 
                     <div className="field half first">
@@ -26,6 +28,32 @@ export default class Contact extends React.Component {
                         <li><input type="reset" value="Clear" /></li>
                     </ul>
                 </form>
+            </section>
+            <section className="split">
+                <section>
+                    <div className="contact-method">
+                        <span className="icon alt fa-envelope"></span>
+                        <h3>Email</h3>
+                        <a href="#">information@untitled.tld</a>
+                    </div>
+                </section>
+                <section>
+                    <div className="contact-method">
+                        <span className="icon alt fa-phone"></span>
+                        <h3>Phone</h3>
+                        <span>(000) 000-0000 x12387</span>
+                    </div>
+                </section>
+                <section>
+                    <div className="contact-method">
+                        <span className="icon alt fa-home"></span>
+                        <h3>Address</h3>
+                        <span>1234 Somewhere Road #5432<br />
+                        Nashville, TN 00000<br />
+                        United States of America</span>
+                    </div>
+                </section>
+            </section>
       </div>
     )
   }
