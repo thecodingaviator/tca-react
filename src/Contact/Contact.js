885c57561181f4adece8786622d12eff0d16f951
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2018-12-18 21:24:09 +0530

    Update Contact.js

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -11,6 +11,21 @@ export default class Contact extends React.Component {
           <input type="email" name="email" />
           <textarea name="message"></textarea>
         </form>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </div>
     )
   }
