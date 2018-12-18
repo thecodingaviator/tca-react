7142f80eb4e8386096adcc66de02764a25b051c3
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2018-12-18 21:32:33 +0530

    Update Contact.js

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -6,26 +6,27 @@ export default class Contact extends React.Component {
     return (
       <div className="contact">
         <h1>Reach Me</h1>
-        <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-          <input type="text" name="name" />
-          <input type="email" name="email" />
-          <textarea name="message"></textarea>
-        </form>
-        <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+        <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
+
+                    <input type="hidden" name="bot-field" />
+
+                    <div className="field half first">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" required/>
+                    </div>
+                    <div className="field half">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" id="email" required/>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" rows="6" required></textarea>
+                    </div>
+                    <ul className="actions">
+                        <li><input type="submit" value="Send Message" className="special" /></li>
+                        <li><input type="reset" value="Clear" /></li>
+                    </ul>
+                </form>
       </div>
     )
   }
